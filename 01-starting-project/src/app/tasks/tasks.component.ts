b89108c8230c1be2d4e5389ff7bf73c6b52a7d9a
Sam <sandeepakula92@gmail.com>
2025-04-05 15:53:15 -0400

    completed tasks app

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTasksComponent } from './new-tasks/new-tasks.component';
 import { NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -11,62 +12,23 @@ import { NewTaskData } from './task/task.model';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-@Input({required: true}) name!: string;
-@Input({required: true}) userId!: string;
-// @Output() newTasks = new EventEmitter<string>();
-isAddingTasks= false;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+  // @Output() newTasks = new EventEmitter<string>();
+  isAddingTasks = false;
 
- dummyTasks = [
-  {
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary:
-      'Learn all the basic and advanced features of Angular & how to apply them.',
-    dueDate: '2025-12-31',
-  },
-  {
-    id: 't2',
-    userId: 'u3',
-    title: 'Build first prototype',
-    summary: 'Build a first prototype of the online shop website',
-    dueDate: '2024-05-31',
-  },
-  {
-    id: 't3',
-    userId: 'u3',
-    title: 'Prepare issue template',
-    summary:
-      'Prepare and describe an issue template which will help with project management',
-    dueDate: '2024-06-15',
-  },
-];
+  constructor(private tasksService: TasksService) { }
 
   get selectedUserTasks() {
-    return this.dummyTasks.filter((task) => task.userId === this.userId)
-  }
- 
-  onComplete(id: string) {
-    this.dummyTasks = this.dummyTasks.filter((tasks) => tasks.id != id)
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTasks() {
     // return this.newTasks.emit("New tasks Added!!");
-    this.isAddingTasks= true;
+    this.isAddingTasks = true;
   }
 
-  onCancelAddTasks() {
-    this.isAddingTasks= false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.dummyTasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.dueDate
-    })
-    this.isAddingTasks= false;
+  onCloseAddTasks() {
+    this.isAddingTasks = false;
   }
 }
